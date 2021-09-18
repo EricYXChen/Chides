405772811532b89b55fceb945dc361a18b0e43ea
Alex Yuan <36246959+alexwhyy@users.noreply.github.com>
2021-09-18 17:56:11 -0400

    Get prebuilt UI working

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,14 +1,32 @@
+import { useEffect } from "react";
+
+import { Container, Card } from "@mui/material";
+
 import * as firebase from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, EmailAuthProvider, GithubAuthProvider } from "firebase/auth";
+
+import "firebaseui/dist/firebaseui.css";
 
 import Layout from "../components/Layout";
 import firebaseConfig from "../src/firebaseConfig";
 
+const app = firebase.initializeApp(firebaseConfig);
+
 export default function Auth() {
+  useEffect(async () => {
+    // delay the import until window object is ready
+    const firebaseui = await import("firebaseui");
+    var ui = new firebaseui.auth.AuthUI(getAuth(app));
+    ui.start("#firebaseui-auth-container", {
+      signInSuccessUrl: "/app",
+      signInOptions: [EmailAuthProvider.PROVIDER_ID],
+      // Other config options...
+    });
+  }, []);
+
   return (
     <Layout>
-      <h1>My App</h1>
-      <p>Please sign-in:</p>
+      <div id="firebaseui-auth-container" style={{ marginTop: 50 }} />
     </Layout>
   );
 }
