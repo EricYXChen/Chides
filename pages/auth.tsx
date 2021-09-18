836f9e88878792b448ea604829454f93ea45b54e
root <root@David-PC.localdomain>
2021-09-18 15:47:43 -0400

    Incomplete login page

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,14 +1,43 @@
-import * as firebase from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+import firebase from "firebase";
 import Layout from "../components/Layout";
 import firebaseConfig from "../src/firebaseConfig";
+import  StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
-export default function Auth() {
+const config = firebaseConfig; 
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}else {
+  firebase.app(); // if already initialized, use that one
+}
+
+// Configure FirebaseUI.
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: 'popup',
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: '/signedIn',
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+  ],
+};
+
+function Auth() {
   return (
-    <Layout>
-      <h1>My App</h1>
-      <p>Please sign-in:</p>
-    </Layout>
+    <div>
+      <Layout>
+        <h1>My App</h1>
+        <p>Please sign-in:</p>
+        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+      </Layout>
+    </div>
   );
 }
+
+export default Auth
+
+
+  
