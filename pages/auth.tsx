3b0fd0fe5bd498a1995ddf2e42fced05cfb018ea
root <root@David-PC.localdomain>
2021-09-18 17:17:04 -0400

    Created login page UI incomplete

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,43 +1,59 @@
 import { getAuth } from "firebase/auth";
+import React, { useRef } from "react"
 import firebase from "firebase";
 import Layout from "../components/Layout";
 import firebaseConfig from "../src/firebaseConfig";
 import  StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import { Container , TextField, Box, Button } from "@mui/material";
 
-const config = firebaseConfig; 
+export default function auth(){
+  const handleSubmit = () => {
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(config);
-}else {
-  firebase.app(); // if already initialized, use that one
+  }
+  return(
+    <>
+      <Container component = "main" maxWidth = "xs">
+          <Box
+            component="form"
+            sx={{
+              '& > :not(style)': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
+            alignItems = 'center'
+            onSubmit = {handleSubmit}
+            >
+            <div>
+            <h1>Sign Up</h1>
+              <TextField 
+                required 
+                fullWidth
+                id = "outlined-required"
+                label = "First Name"
+              />
+            </div>
+            <div>
+              <TextField 
+                required 
+                fullWidth
+                id = "outlined-required"
+                label = "Last Name"
+              />
+            </div>
+            <div>
+              <TextField 
+                required 
+                fullWidth
+                id = "outlined-required"
+                label = "Email"
+              />
+            </div>
+            <div>
+              <Button type="submit" fullWidth variant = "outlined">Sign Up</Button>
+            </div>
+          </Box>
+        </Container>
+    </>
+  )
 }
-
-// Configure FirebaseUI.
-const uiConfig = {
-  // Popup signin flow rather than redirect flow.
-  signInFlow: 'popup',
-  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-  signInSuccessUrl: '/signedIn',
-  // We will display Google and Facebook as auth providers.
-  signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-  ],
-};
-
-function Auth() {
-  return (
-    <div>
-      <Layout>
-        <h1>My App</h1>
-        <p>Please sign-in:</p>
-        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
-      </Layout>
-    </div>
-  );
-}
-
-export default Auth
-
-
   
